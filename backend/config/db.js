e75b78c9e327c8c159f3e0ceb382bd31b1582177
@yogeshwargopi <112958432+yogeshwargopi@users.noreply.github.com>
2024-06-26 19:27:24 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -13,4 +13,12 @@ const sequelize = new Sequelize(
   }
 );
 
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 module.exports = sequelize;
